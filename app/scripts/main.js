7fae80e01e41d0d1576eec825e572c0b25643438
Jeremy Crawford <jcrawford.design@gmail.com>
2014-06-26 06:43:28 -0700

    added new logo and header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,19 @@ document.querySelector( "#nav-toggle" )
   .addEventListener( "click", function() {
     this.classList.toggle( "active" );
   });
+
+  $(function () {
+    $('.click-nav > ul').toggleClass('no-js js');
+    $('.click-nav .js ul').hide();
+    $('.click-nav .js').click(function(e) {
+      $('.click-nav .js ul').slideToggle(200);
+      $('#nav-toggle').toggleClass('active');
+      e.stopPropagation();
+    });
+    $(document).click(function() {
+      if ($('.click-nav .js ul').is(':visible')) {
+        $('.click-nav .js ul', this).slideUp();
+        $('#nav-toggle').removeClass('active');
+      }
+    });
+  });
