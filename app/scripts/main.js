bc8f0eed18918df35641189a6165899820f19327
Jeremy Crawford <jcrawford.design@gmail.com>
2014-06-28 00:11:15 -0700

    added javascript

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,38 @@
 console.log('\'Allo \'Allo!');
 
-document.querySelector( "#nav-toggle" )
-  .addEventListener( "click", function() {
-    this.classList.toggle( "active" );
-  });
+$("#home-button").click(function() {
+    $('body').animate({
+        scrollTop: $("#main").offset().top
+    }, 1250);
+});
 
-  $(function () {
-    $('.click-nav > ul').toggleClass('no-js js');
-    $('.click-nav .js ul').hide();
-    $('.click-nav .js').click(function(e) {
-      $('.click-nav .js ul').slideToggle(200);
-      $('#nav-toggle').toggleClass('active');
-      e.stopPropagation();
-    });
-    $(document).click(function() {
-      if ($('.click-nav .js ul').is(':visible')) {
-        $('.click-nav .js ul', this).slideUp();
-        $('#nav-toggle').removeClass('active');
-      }
-    });
-  });
+$("#work-button").click(function() {
+    $('body').animate({
+        scrollTop: $("#work-header").offset().top
+    }, 1250);
+});
+
+$("#about-button").click(function() {
+    $('body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+});
+
+$("#contact-button").click(function() {
+    $('body').animate({
+        scrollTop: $("#footer").offset().top
+    }, 750);
+});
+
+$("#nav-arrow").click(function() {
+    $('body').animate({
+        scrollTop: $("#work-header").offset().top
+    }, 1250);
+});
+
+// $(function () {
+// $("#work-button").click(function (e) {
+// e.preventDefault();
+// $("#work-button").addClass("active").not(this).removeClass("active");
+// });
+// });
